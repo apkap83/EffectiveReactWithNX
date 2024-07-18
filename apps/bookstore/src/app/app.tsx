14d36aacab3cf97081f3d8dbe68e2762a938a585
apkap83 <33512036+apkap83@users.noreply.github.com>
2024-07-18 17:19:18 +0300

    add books feature and ui components

--- apps/bookstore/src/app/app.tsx
+++ apps/bookstore/src/app/app.tsx
@@ -1,5 +1,14 @@
 import styled from 'styled-components';
 
+import { Route, Routes, Link, useNavigate } from 'react-router-dom';
+import {
+  GlobalStyles,
+  Header,
+  Main,
+  NavigationItem,
+  NavigationList,
+} from '@acme/ui';
+import { BooksFeature } from '@acme/feature';
 // import NxWelcome from './nx-welcome';
 
 const StyledApp = styled.div`
@@ -7,13 +16,40 @@ const StyledApp = styled.div`
 `;
 
 export function App() {
+  let navigate = useNavigate();
   return (
-    <StyledApp>
-      <header>
+    <>
+      <GlobalStyles />
+      <Header>
         <h1>Bookstore</h1>
-      </header>
+        <NavigationList>
+          <NavigationItem>
+            <Link to="/books">Books</Link>
+          </NavigationItem>
+        </NavigationList>
+      </Header>
       {/* <NxWelcome title="bookstore" /> */}
-    </StyledApp>
+
+      {/* START: routes */}
+      {/* These routes and navigation have been generated for you */}
+      {/* Feel free to move and update them to fit your needs */}
+      <br />
+      <hr />
+      <br />
+      <Routes>
+        <Route path="/" element={<BooksFeature />} />
+        <Route path="/books" element={<BooksFeature />} />
+        <Route
+          path="/page-2"
+          element={
+            <div>
+              <Link to="/">Click here to go back to root page.</Link>
+            </div>
+          }
+        />
+      </Routes>
+      {/* END: routes */}
+    </>
   );
 }
 
---
+++ books/src/lib/feature.tsx
@@ -0,0 +1,15 @@
+import styled from 'styled-components';
+
+const StyledFeature = styled.div`
+  color: pink;
+`;
+
+export function BooksFeature() {
+  return (
+    <StyledFeature>
+      <h1>Welcome to BooksFeature!</h1>
+    </StyledFeature>
+  );
+}
+
+export default BooksFeature;
