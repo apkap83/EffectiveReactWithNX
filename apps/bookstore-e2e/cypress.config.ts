4689a6dfd434d68177bc1b7a35c4a4276e4470d0
apkap83 <33512036+apkap83@users.noreply.github.com>
2024-07-18 12:00:46 +0300

    Initial commit

---
+++ apps/bookstore-e2e/cypress.config.ts
@@ -0,0 +1,17 @@
+import { nxE2EPreset } from '@nx/cypress/plugins/cypress-preset';
+
+import { defineConfig } from 'cypress';
+
+export default defineConfig({
+  e2e: {
+    ...nxE2EPreset(__filename, {
+      cypressDir: 'src',
+      webServerCommands: {
+        default: 'nx run bookstore:serve',
+        production: 'nx run bookstore:preview',
+      },
+      ciWebServerCommand: 'nx run bookstore:serve-static',
+    }),
+    baseUrl: 'http://localhost:4200',
+  },
+});
